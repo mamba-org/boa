d42867647c0b2b66649611e0d9a2b0ca39e6ba6f
Madhur Tandon <madhur16053@iiitd.ac.in>
2021-11-02 23:51:42 +0530

    test both grayskull and ipywidgets in one test

--- tests/test_boa_build.py
+++ tests/test_boa_build.py
@@ -2,24 +2,13 @@ import pathlib
 from subprocess import check_call
 
 
-def test_build_grayskull():
+def test_build_recipes():
     recipes_dir = pathlib.Path(__file__).parent / "recipes-v2"
 
     recipes = [str(x) for x in recipes_dir.iterdir() if x.is_dir()]
 
     for recipe in recipes:
-        if "grayskull" in recipe:
-            check_call(["boa", "build", recipe])
-
-
-def test_build_ipywidgets():
-    recipes_dir = pathlib.Path(__file__).parent / "recipes-v2"
-
-    recipes = [str(x) for x in recipes_dir.iterdir() if x.is_dir()]
-
-    for recipe in recipes:
-        if "ipywidgets" in recipe:
-            check_call(["boa", "build", recipe])
+        check_call(["boa", "build", recipe])
 
 
 def test_build_notest():
