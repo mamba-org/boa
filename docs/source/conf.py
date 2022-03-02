d5f08825c60e5aeb7d3f027cff56d61fa11e7494
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-03-02 07:39:16 +0100

    more validation and more proper handling of some build options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ release = version_ns["__version__"]
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser"]
+extensions = ["myst_parser", "sphinx-jsonschema"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
---
+++ docs/source/jsonschema_spec.md
@@ -0,0 +1,7 @@
+# Spec generated from JSON schema
+
+This represents the spec that is used to validate recipes against.
+
+
+```{jsonschema} ../../boa/schemas/recipe.v1.json
+```
