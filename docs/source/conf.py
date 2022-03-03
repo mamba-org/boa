e29aca2c458c211627874a568de3d71545881f27
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-03-03 16:06:42 +0100

    make scheme render more nicely

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+
 html_theme = "sphinx_book_theme"
 html_logo = "_static/boa.svg"
 html_title = "documentation"
@@ -59,3 +60,6 @@ html_title = "documentation"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = [
+    "style.css",
+]
