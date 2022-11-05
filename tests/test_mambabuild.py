f93b40d695fa061927e3810620b34b1f86a5b3ed
Marcel Bargull <marcel.bargull@udo.edu>
2022-11-05 11:33:09 +0100

    tests: Split out mambabuild dependency error tests

--- tests/test_mambabuild.py
+++ tests/test_mambabuild.py
@@ -4,18 +4,31 @@ from subprocess import check_call, CalledProcessError
 
 recipes_dir = Path(__file__).parent / "recipes"
 
-recipes = [str(x) for x in recipes_dir.iterdir() if x.is_dir()]
-notest_recipes = [x for x in recipes if Path(x).name in ["baddeps"]]
+dep_error_recipes = [
+    str(recipes_dir / name) for name in (
+        "baddeps",
+        "dep_error_nothing_provides",
+        "dep_error_needed_by",
+        "dep_error_package_requires",
+        "dep_error_has_constaint",
+    )
+]
+recipes = [
+    str(x) for x in recipes_dir.iterdir()
+    if x.is_dir() and str(x) not in dep_error_recipes
+]
+notest_recipes = [str(recipes_dir / "baddeps")]
+
+
+@pytest.mark.parametrize("recipe", dep_error_recipes)
+def test_build_dep_error_recipes(recipe):
+    with pytest.raises(CalledProcessError):
+        check_call(["conda", "mambabuild", recipe])
 
 
 @pytest.mark.parametrize("recipe", recipes)
 def test_build_recipes(recipe):
-    expected_fail_recipes = ["baddeps"]
-    if Path(recipe).name in expected_fail_recipes:
-        with pytest.raises(CalledProcessError):
-            check_call(["conda", "mambabuild", recipe])
-    else:
-        check_call(["conda", "mambabuild", recipe])
+    check_call(["conda", "mambabuild", recipe])
 
 
 @pytest.mark.parametrize("recipe", notest_recipes)
